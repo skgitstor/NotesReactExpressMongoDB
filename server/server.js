07d0a80e675b2eb9471ee99aa61c27e56a9150ae
skgitstor <164068773+skgitstor@users.noreply.github.com>
2026-09-21 23:45:00 +0530

    feat(server): connect mongodb and add user & note models
    Co-authored-by: skgitstor <143597388+djgitstor@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,7 +1,50 @@
 import Express from "express"
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors"
-// mongoose.connect("mongodb://localhost:27017/reactNotesApp");
+mongoose.connect("mongodb://localhost:27017/react_notes_db").then(() => {
+    console.log("mongodbConnected...")
+}).catch((err) => {
+    console.log(`error : ${err}`)
+})
+
+const noteSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // 'User' model se relation link karta hai
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        default: ''
+    }
+}, { timestamps: true }); // created_at aur updated_at apne aap add ho jayenge
+const Note = mongoose.model('Note', noteSchema);
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Do users same email use nahi kar sakte
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
 const app = Express();
 const corsOptions = {
     "origin": "http://localhost:5173",
